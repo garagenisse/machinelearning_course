90a674456eb3abf7412c092b9f1b8b7ac45663d4
Andreas Nilsson <garagenisse@hotmail.com>
2016-06-08 22:46:04 +0200

    Added Week 6 lab 5 source, vectorized backpropagation algo

--- Week 5/machine-learning-ex4/ex4/nnCostFunction.m
+++ Week 5/machine-learning-ex4/ex4/nnCostFunction.m
@@ -86,22 +86,16 @@ K = num_labels;
 
 delta_accum_1 = zeros(size(Theta1));
 delta_accum_2 = zeros(size(Theta2));
+ym = zeros(m,K);
 
 for t = 1:m
-	a_1 = X(t,:);  
-	z_2 = a_1 * Theta1';
-	a_2 = [1 sigmoid(z_2)];
-	z_3 = a_2 * Theta2';
-	a_3 = sigmoid(z_3);
-	y_i = zeros(1,K);
-	y_i(y(t)) = 1;
-	
-	delta_3 = a_3 - y_i;
-	delta_2 = delta_3 * Theta2 .* sigmoidGradient([1 z_2]);
-	
-	delta_accum_1 = delta_accum_1 + delta_2(2:end)' * a_1;
-	delta_accum_2 = delta_accum_2 + delta_3' * a_2;
-end;
+  ym(t,y(t)) = 1;
+end
+
+delta_3 = a3 - ym;
+delta_2 = delta_3 * Theta2 .* sigmoidGradient([ones(m,1), z2]);
+delta_accum_1 = delta_accum_1 + delta_2(:,2:end)' * X;
+delta_accum_2 = delta_accum_2 + delta_3' * a2;
 
 Theta1_grad = delta_accum_1 / m;
 Theta2_grad = delta_accum_2 / m;
