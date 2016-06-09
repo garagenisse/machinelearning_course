7bea63232cc4a8a1b012370e5310d231e6afeac2
garagenisse <garagenisse@hotmail.com>
2016-06-09 13:58:26 +0200

    Week 6 Lab 5 Complete

--- Week 6/machine-learning-ex5/ex5/learningCurve.m
+++ Week 6/machine-learning-ex5/ex5/learningCurve.m
@@ -53,6 +53,16 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+
+   % Compute train/cross validation errors using training examples 
+   % function [theta] = trainLinearReg(X, y, lambda)
+   theta = trainLinearReg(X(1:i,:),y(1:i),lambda);
+
+   [error_train(i), grad_train ] =  linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+   [error_val(i), grad_val ] =  linearRegCostFunction(Xval, yval, theta, 0);
+   
+end
 
 
 
